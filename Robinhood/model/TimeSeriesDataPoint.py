617270788af75d15530f4b80704b0e4a4844f513
pkim49 <pjkim@gatech.edu>
2018-04-03 16:27:00 -0400

    adding some getters to data point model

--- Robinhood/model/TimeSeriesDataPoint.py
+++ Robinhood/model/TimeSeriesDataPoint.py
@@ -1,6 +1,7 @@
 class TimeSeriesDataPoint:
 
     def __init__(self, date, volume, close, high, open, low):
+        self.date = date
         self.volume = volume
         self.close_price = close
         self.high_price = high
